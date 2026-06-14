9033100f5ef0d95bdb8ec740c023ce84a28eaf54
ayuuuuu0-0 <ranaayush0725@gmail.com>
2026-06-15 01:08:53 +0530

    heap, tree

--- tree/create-binary-tree-by-description.cpp
+++ tree/create-binary-tree-by-description.cpp
@@ -39,3 +39,37 @@ public:
         return NULL; //we will never reach here
     }
 };
+
+// brute force /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        vector<int>vec;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            vec.push_back(curr->val);
+            curr = curr -> next;
+        }
+
+        int i=0;
+        int j=vec.size()-1;
+        int result = 0;
+
+        while (i<j){
+            result = max(result, vec[i] + vec[j]);
+            i++;
+            j--; 
+        }
+return result;
+    }
+};
