ca4a3b66af0d9b9f22d49f74ce3366e44792f595
ayuuuuu0-0 <ranaayush0725@gmail.com>
2026-07-13 23:59:57 +0530

    graph

---
+++ graph/course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool topologicalSort(unordered_map<int, vector<int>>adj, int n, vector<int>& indegree){
+
+    queue<int>q;
+    int count = 0;
+
+    for(int i=0; i<n; i++){
+        if (indegree[i] == 0){
+            q.push(i);
+            count++;
+        }
+    }
+
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+
+        for(int &v: adj[u]){
+            indegree[v]--;
+            if(indegree[v] == 0){
+                count++;
+                q.push(v);
+            }
+        }
+    }
+    if(count == n) return true;
+    return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        vector<int>indegree(numCourses, 0);
+
+        for(auto &vec: prerequisites){
+            int a = vec[0];
+            int b = vec[1];
+
+            // b ---> a arrow a ki side hai toh push_back in adj
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+        return topologicalSort(adj, numCourses, indegree);
+    }
+};
