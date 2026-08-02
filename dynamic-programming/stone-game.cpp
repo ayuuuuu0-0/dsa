60bddef3ef073eabb960c6bf91b2fdd6540bc5a5
Ayush Ranjan <ranaayush0725@gmail.com>
2026-08-02 23:41:16 +0530

    potd

---
+++ dynamic-programming/stone-game.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+private:
+    int memo[505][505];
+
+    int solve(vector<int> &piles, int i, int j)
+    {
+        if (i > j)
+            return 0;
+
+        int(memo[i][j] != -1)
+    }
+}
