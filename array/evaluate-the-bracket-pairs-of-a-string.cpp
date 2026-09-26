02967e9717be88ff4a79f0d9f8972de91d33de5f
Ayush Ranjan <ranaayush0725@gmail.com>
2026-09-26 23:56:23 +0530

    potd

---
+++ array/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    // Code 3
+    string evaluate(string s, vector<vector<string>> &knowledge)
+    {
+        int n = s.length();
+
+        unordered_map<string, string> mp;
+
+        for (auto &vec : knowledge)
+        {
+            // vec[0], vec[1]
+            mp[vec[0]] = vec[1];
+        }
+
+        string result = "";
+        int i = 0;
+
+        while (i < n)
+        {
+            if (s[i] == '(')
+            {
+                int j = s.find(")", i + 1);
+                string temp = s.substr(i + 1, j - i - 1);
+                result += mp.count(temp) ? mp[temp] : "?";
+                i = j;
+            }
+            else
+            {
+                result.push_back(s[i]);
+            }
+
+            i++;
+        }
+        return result;
+    }
+};
